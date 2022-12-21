efdea330d6140f7fcb5946397726898bddd6c251
Denis <mdr2010@yandex.ru>
2022-12-21 12:04:56 +0500

    add Task13

---
+++ HomeWork2/Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+int a = new Random().Next(1, 1000000);
+Console.WriteLine(a);
+string b = a.ToString();
+int n = b.Length;
+if (n > 2)
+{
+    Console.WriteLine(b[2]);
+}
+else
+{
+    Console.WriteLine($"третьей цифры нет");
+}
